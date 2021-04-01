af942ba5edc20b1a5e320cca2afc8d22c5fb412e
itas13 <mumtazul.fuadi13@gmail.com>
2021-04-01 17:15:20 +0700

    Pesan komit wajib diisi massage : event onclik listener (update)

--- app/src/main/java/com/example/constraintlayout/MainActivity.java
+++ app/src/main/java/com/example/constraintlayout/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.constraintlayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -80,5 +83,23 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Method untuk menam[ilkan menu.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //Membuat kondisi jika yang dipilih adalah id mnDaftar
+        if (item.getItemId() == R.id.mnDaftar){
+            //Method untuk memanggil activity "DaftarActivity"
+            Intent i =new Intent(getApplicationContext(), FormPendaftaran.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
